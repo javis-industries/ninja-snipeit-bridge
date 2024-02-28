d873446752eade2406b7a72168c20b806b942ac6
Christopher Durjak <c.durjak@gmail.com>
2024-02-28 10:35:16 -0600

    Added SQLite Database and built database

---
+++ SQL/databaseinit.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const sqlite3 = require('sqlite3').verbose();
+
+const dbFile = 'NinjaSnipeITBridge.db';
+
+// Check if the database file exists, and delete it if it does
+if (fs.existsSync(dbFile)) {
+    fs.unlinkSync(dbFile);
+    console.log(`Deleted the old database file '${dbFile}'.`);
+}
+
+// Create a new SQLite database
+const db = new sqlite3.Database(dbFile, (err) => {
+    if (err) {
+        console.error(err.message);
+    } else {
+        console.log(`SQLite database file '${dbFile}' has been created.`);
+    }
+});
+
+db.run(`
+  CREATE TABLE IF NOT EXISTS devices (
+    assetTag INTEGER PRIMARY KEY UNIQUE,
+    ninjaId INTEGER NOT NULL UNIQUE,
+    snipeitId INTEGER NOT NULL UNIQUE,
+    serialNumber TEXT NOT NULL UNIQUE
+  )
+`);
+
+// Close the database connection
+db.close((err) => {
+    if (err) {
+        console.error(err.message);
+    }
+});
